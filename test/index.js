c2ef9750c3304e7245245a3d88c3c990004834aa
Eran Hammer <eran@hueniverse.com>
2013-04-01 17:00:25 -0700

    migrate to lab

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 // Load modules
 
-var Chai = require('chai');
+var Lab = require('lab');
 var Fs = require('fs');
 var Faketoe = require('../lib');
 
@@ -12,7 +12,11 @@ var internals = {};
 
 // Test shortcuts
 
-var expect = Chai.expect;
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
 
 
 describe('XML', function () {
