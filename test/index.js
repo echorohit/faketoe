2a2b597ef01ca065c2fc6194c25533dc5e169bdc
Nathan LaFreniere <quitlahok@gmail.com>
2015-07-08 10:23:39 -0700

    update lab, closes #26

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 // Load modules
 
+var Code = require('code');
 var Lab = require('lab');
 var Fs = require('fs');
 var Faketoe = require('../lib');
@@ -15,7 +16,7 @@ var internals = {};
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
 var it = lab.it;
-var expect = Lab.expect;
+var expect = Code.expect;
 
 
 describe('createParser()', function () {
